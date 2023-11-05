af2bffca75b0dd8aeaae4db07def3affe0aa97be
Nour-Cherrat <cherrat.nour@gmail.com>
2023-11-06 00:44:09 +0100

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import sys
 
     value = 0
-    for arg in sys.argv[1:]:
-        value = value + int(arg)
+    for i in range(len(sys.argv) - 1):
+        value = value + int(sys.argv[i + 1])
     print("{}".format(value))
