45c90c8f9e2c086a67a10d65f34cd878ca1620c7
Nour-Cherrat <cherrat.nour@gmail.com>
2023-11-06 00:45:24 +0100

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
+    built = dir(hidden_4)
+    for name in built:
+        if name[:2] != "__":
             print(name)
